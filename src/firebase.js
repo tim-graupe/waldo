3700c54a9e4923613ac75303f70343b98bb02ddd
tim-graupe <tgraupe90@gmail.com>
2022-08-29 22:22:18 -0400

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyD0rgSi8JAMT1AUy1jXeKUCITqNjoXSuRI",
+  
+    authDomain: "waldo-648ff.firebaseapp.com",
+  
+    projectId: "waldo-648ff",
+  
+    storageBucket: "waldo-648ff.appspot.com",
+  
+    messagingSenderId: "662044481458",
+  
+    appId: "1:662044481458:web:16b6201d065a4a3791f6ab"
+  
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+export default firebase;
